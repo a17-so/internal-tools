4281d52d459e0bc2d637f2f51d1934419030d9a2
Abhay Chebium <abhaychebium@gmail.com>
2025-09-05 16:19:00 -0700

    feat: script update

--- outreach-tool/api/scripts/pretti.py
+++ outreach-tool/api/scripts/pretti.py
@@ -7,7 +7,7 @@ TEMPLATES = {
             "Pretti is an AI beauty app that gives you step-by-step guides to recreate any makeup look and helps you shop for the right products that match your style and skin.\n\n"
             "we're inviting a few, **bigtime beauty creators** who have a **cultlike following** to try it out. if you vibe with it we want to pay you to promote the app :)\n\n"
             "Let me know if you're interested!\n\n"
-            "-**Abhay Chebium** from the [Pretti App](https://https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
+            "-**Abhay Chebium** from the [Pretti App](https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
         ),
         "dm_md": (
             "hey {name},\n\n"
@@ -26,7 +26,7 @@ TEMPLATES = {
             "Pretti is an AI beauty app that gives you step-by-step guides to recreate any makeup look and helps you shop for the right products that match your style and skin.\n\n"
             "we're inviting a few, **niche beauty creators** who have a lot of **talent** to try it out. if you vibe with it we want to pay you to promote the app :)\n\n"
             "Let me know if you're interested!\n\n"
-            "-**Abhay Chebium** from the [Pretti App](https://https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
+            "-**Abhay Chebium** from the [Pretti App](https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
         ),
         "dm_md": (
             "hey {name},\n\n"
@@ -45,7 +45,7 @@ TEMPLATES = {
             "Pretti is an AI beauty app that gives you step-by-step guides to recreate any makeup look and helps you shop for the right products that match your style and skin.\n\n"
             "we're looking for creators with **great energy** to make brand new tiktok accounts and post 15 videos per week for $400. I think you'd be a great fit for it! would you be interested?\n\n"
             "Let me know if you're interested!\n\n"
-            "-**Abhay Chebium** from the [Pretti App](https://https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
+            "-**Abhay Chebium** from the [Pretti App](https://apps.apple.com/us/app/pretti-ai-makeup-assistant/id6749188903)\n"
         ),
         "dm_md": (
             "hey {name},\n\n"
